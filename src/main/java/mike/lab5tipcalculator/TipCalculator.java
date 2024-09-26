c404c5ba8cea6c1fb2e404c45ee6daa0eb58df86
molew-creator <molew679@gmail.com>
2024-09-26 13:43:20 -0400

    Second commit. Changed stage title.

--- src/main/java/mike/lab5tipcalculator/TipCalculator.java
+++ src/main/java/mike/lab5tipcalculator/TipCalculator.java
@@ -13,7 +13,7 @@ public class TipCalculator extends Application {
                 FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
 
         Scene scene = new Scene(root); // attach scene graph to scene
-        stage.setTitle("Tip Calculator"); // displayed in window's title bar
+        stage.setTitle("Mike's Tip Calculator"); // displayed in window's title bar
         stage.setScene(scene); // attach scene to stage
         stage.show(); // display the stage
     }
